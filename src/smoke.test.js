b65997dec33c3732c2c3618d0bdce584be877529
ninevra <ninevra@gmail.com>
2021-01-22 16:32:55 -0800

    Snapshot the module API

--- src/smoke.test.js
+++ src/smoke.test.js
@@ -1,15 +1,5 @@
 import test from 'ava';
 
 test('import() should import all classes, messages', async (t) => {
-  const mod = await import('crystal-query');
-  t.true(typeof mod.Parser === 'function');
-  t.true(typeof mod.Schema === 'function');
-  t.true(typeof mod.PropError === 'function');
-  t.true(typeof mod.FieldTermHandler === 'function');
-  t.true(typeof mod.NumberPropertyField === 'function');
-  t.true(typeof mod.StringPropertyField === 'function');
-  t.true(typeof mod.StringArrayPropertyField === 'function');
-  t.truthy(mod.messages);
-  t.true(typeof mod.messages.fieldGeneric === 'function');
-  t.falsy(mod.default);
+  t.snapshot(await import('crystal-query'));
 });
