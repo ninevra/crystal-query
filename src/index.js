66fae14e1da2edb443a6920c2d7f3dc213b80d7c
ninevra <ninevra@gmail.com>
2022-10-27 03:08:59 -0700

    Remove all interfaces except parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 export { Parser } from './parser.js';
-export { Schema, PropError } from './schema.js';
-export * as messages from './messages.js';
-export { FieldTermHandler } from './terms/FieldTermHandler.js';
-export { NumberPropertyField } from './terms/NumberPropertyField.js';
-export { StringPropertyField } from './terms/StringPropertyField.js';
-export { StringArrayPropertyField } from './terms/StringArrayPropertyField.js';
--- src/terms/StringArrayPropertyField.test.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import { StringArrayPropertyField } from './StringArrayPropertyField.js';
-
-test('supports ":"', (t) => {
-  const field = new StringArrayPropertyField('some strings', true, 'foo');
-  t.is(field[':']('bar').describe(), 'some strings contain "bar"');
-  t.true(field[':']('bar').predicate({ foo: ['bar', 'baz'] }));
-  t.false(field[':']('bar').predicate({ foo: ['baz'] }));
-});
