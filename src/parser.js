7168bd3a1d03d9c31a40b5b94e9b04759c9a1eb4
ninevra <ninevra@gmail.com>
2022-10-28 16:03:55 -0700

    Collapse invalid constructions
    Probably this should be done optionally, in post-processing?
    
    Makes the AST much simpler without losing any useful info.

--- src/parser.js
+++ src/parser.js
@@ -17,6 +17,26 @@ function node(name) {
     parser.node(name).map(({ value, ...rest }) => ({ ...rest, ...value }));
 }
 
+function collapseBinary(node) {
+  if (node.left === undefined) {
+    return node.right;
+  }
+
+  if (node.right === undefined) {
+    return node.left;
+  }
+
+  return node;
+}
+
+function collapseUnary(node) {
+  if (node.expression === undefined) {
+    return node.expression;
+  }
+
+  return node;
+}
+
 export class Parser {
   constructor() {
     this.language = parsimmon.createLanguage({
@@ -42,12 +62,6 @@ export class Parser {
         )
           .map(([field, operator, value]) => ({ field, operator, value }))
           .thru(node('Term')),
-      nil: () =>
-        succeed({
-          field: undefined,
-          operator: undefined,
-          value: undefined
-        }).thru(node('Term')),
       parenthetical: (l) =>
         seqObj(
           string('('),
@@ -55,29 +69,33 @@ export class Parser {
           ['expression', l.optExpression],
           optWhitespace,
           string(')')
-        ).thru(node('Parenthetical')),
+        )
+          .thru(node('Parenthetical'))
+          .map((node) => collapseUnary(node)),
       basic: (l) => alt(l.term, l.parenthetical),
       negation: (l) =>
         alt(
-          seqObj(l.not, optWhitespace, ['expression', l.optNegation]).thru(
-            node('Not')
-          ),
+          seqObj(l.not, optWhitespace, ['expression', l.optNegation])
+            .thru(node('Not'))
+            .map((node) => collapseUnary(node)),
           l.basic
         ),
-      optNegation: (l) => alt(l.negation, l.nil),
+      optNegation: (l) => alt(l.negation, l.nothing),
       conjunction: (l) =>
         alt(
           seqObj(['left', l.optNegation], optWhitespace, l.and, optWhitespace, [
             'right',
             l.optConjunction
-          ]).thru(node('And')),
+          ])
+            .thru(node('And'))
+            .map((node) => collapseBinary(node)),
           seqObj(['left', l.negation], optWhitespace, [
             'right',
             l.conjunction
           ]).thru(node('And')),
           l.negation
         ),
-      optConjunction: (l) => alt(l.conjunction, l.nil),
+      optConjunction: (l) => alt(l.conjunction, l.nothing),
       disjunction: (l) =>
         alt(
           seqObj(
@@ -86,12 +104,14 @@ export class Parser {
             l.or,
             optWhitespace,
             ['right', l.optDisjunction]
-          ).thru(node('Or')),
+          )
+            .thru(node('Or'))
+            .map((node) => collapseBinary(node)),
           l.conjunction
         ),
-      optDisjunction: (l) => alt(l.disjunction, l.nil),
+      optDisjunction: (l) => alt(l.disjunction, l.nothing),
       expression: (l) => l.disjunction,
-      optExpression: (l) => alt(l.expression, l.nil),
+      optExpression: (l) => alt(l.expression, l.nothing),
       query: (l) => l.optExpression.trim(optWhitespace)
     });
   }
