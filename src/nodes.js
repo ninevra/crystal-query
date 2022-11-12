12064d38ce9df2ceb0a29001e92a7c058adb0dd8
ninevra <ninevra@gmail.com>
2022-11-11 22:24:19 -0800

    Remove node property setters
    These were not used or tested. In the data model, nodes should
    presently be immutable.

--- src/nodes.js
+++ src/nodes.js
@@ -15,16 +15,8 @@ class Binary extends Node {
     return this.children[0];
   }
 
-  set left(child) {
-    this.children[0] = child;
-  }
-
   get right() {
     return this.children[4];
-  }
-
-  set right(child) {
-    this.children[4] = child;
   }
 }
 
@@ -39,10 +31,6 @@ export class And extends Binary {
   get and() {
     return this.children[2];
   }
-
-  set and(child) {
-    this.children[2] = child;
-  }
 }
 
 export class Or extends Binary {
@@ -56,19 +44,11 @@ export class Or extends Binary {
   get or() {
     return this.children[2];
   }
-
-  set or(child) {
-    this.children[2] = child;
-  }
 }
 
 class Unary extends Node {
   get expression() {
     return this.children[2];
-  }
-
-  set expression(child) {
-    this.children[2] = child;
   }
 }
 
@@ -82,10 +62,6 @@ export class Not extends Unary {
 
   get not() {
     return this.children[0];
-  }
-
-  set not(child) {
-    this.children[0] = child;
   }
 }
 
@@ -101,16 +77,8 @@ export class Group extends Unary {
     return this.children[0];
   }
 
-  set open(child) {
-    this.children[0] = child;
-  }
-
   get close() {
     return this.children[4];
-  }
-
-  set close(child) {
-    this.children[4] = child;
   }
 }
 
@@ -126,24 +94,12 @@ export class Term extends Node {
     return this.children[0];
   }
 
-  set field(child) {
-    this.children[0] = child;
-  }
-
   get operator() {
     return this.children[2];
   }
 
-  set operator(child) {
-    this.children[2] = child;
-  }
-
   get value() {
     return this.children[4];
-  }
-
-  set value(child) {
-    this.children[4] = child;
   }
 }
 
@@ -159,24 +115,12 @@ export class Text extends Node {
     return this.children[0];
   }
 
-  set open(child) {
-    this.children[0] = child;
-  }
-
   get content() {
     return this.children[1];
   }
 
-  set content(child) {
-    this.children[1] = child;
-  }
-
   get close() {
     return this.children[2];
-  }
-
-  set close(child) {
-    this.children[2] = child;
   }
 }
 
