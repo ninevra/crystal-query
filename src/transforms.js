0fdb6706a3461fbd6defee51eb662fcdeb91fe4b
ninevra <ninevra@gmail.com>
2022-11-12 00:31:08 -0800

    Refactor fold()

--- src/transforms.js
+++ src/transforms.js
@@ -1,194 +1,181 @@
 import { And, Or, Not, Group, Literal, Term } from './nodes.js';
 
-export function fold(node, { preVisit = (x) => x, postVisit = (x) => x }) {
-  node = preVisit(node);
-  if (node?.children !== undefined) {
-    node = new node.constructor(node);
-    node.children = node.children.map((node) =>
-      fold(node, { preVisit, postVisit })
-    );
-  }
+function fold2(node, foldfn) {
+  return foldfn(node, (node, fn = foldfn) =>
+    node?.children === undefined
+      ? node
+      : new node.constructor({
+          ...node,
+          children: node.children.map((node) => fold2(node, fn))
+        })
+  );
+}
 
-  return postVisit(node);
+export function fold(node, { preVisit = (x) => x, postVisit = (x) => x }) {
+  return fold2(node, (node, visit) => {
+    return postVisit(visit(preVisit(node)));
+  });
 }
 
 export function leavesToValue(node) {
-  return fold(node, {
-    preVisit(node) {
-      if (
-        typeof node === 'object' &&
-        (node?.name === undefined || node?.name === 'Word')
-      ) {
-        return node?.value;
-      }
-
-      return node;
+  return fold2(node, (node, visit) => {
+    if (
+      typeof node === 'object' &&
+      (node?.name === undefined || node?.name === 'Word')
+    ) {
+      return node?.value;
     }
+
+    return visit(node);
   });
 }
 
 export function textToLiteral(node) {
-  return fold(node, {
-    preVisit(node) {
-      if (node?.name === 'Text') {
-        const {
-          start,
-          end,
-          content: { value, raw }
-        } = node;
-        return new Literal({ start, end, value, raw });
-      }
-
-      return node;
+  return fold2(node, (node, visit) => {
+    if (node?.name === 'Text') {
+      const {
+        start,
+        end,
+        content: { value, raw }
+      } = node;
+      return new Literal({ start, end, value, raw });
     }
+
+    return visit(node);
   });
 }
 
 export function minimizeChildren(node) {
-  return fold(node, {
-    postVisit(node) {
-      switch (node?.name) {
-        case 'And':
-          return new And({ left: node.left, right: node.right });
-        case 'Or':
-          return new Or({ left: node.left, right: node.right });
-        case 'Not':
-          return new Not({ expression: node.expression });
-        case 'Group':
-          return new Group({ expression: node.expression });
-        case 'Term':
-          return new Term({
-            field: node.field,
-            operator: node.operator,
-            value: node.value
-          });
-        default:
-          return node;
-      }
+  return fold2(node, (node, visit) => {
+    node = visit(node);
+    switch (node?.name) {
+      case 'And':
+        return new And({ left: node.left, right: node.right });
+      case 'Or':
+        return new Or({ left: node.left, right: node.right });
+      case 'Not':
+        return new Not({ expression: node.expression });
+      case 'Group':
+        return new Group({ expression: node.expression });
+      case 'Term':
+        return new Term({
+          field: node.field,
+          operator: node.operator,
+          value: node.value
+        });
+      default:
+        return node;
     }
   });
 }
 
 export function removeGroups(node) {
-  return fold(node, {
-    preVisit(node) {
-      if (node?.name === 'Group') {
-        return node.expression;
-      }
-
-      return node;
+  return fold2(node, (node, visit) => {
+    if (node?.name === 'Group') {
+      return visit(node.expression);
     }
+
+    return visit(node);
   });
 }
 
 export function collapseIncomplete(node) {
-  return fold(node, {
-    postVisit(node) {
-      switch (node?.name) {
-        case 'And':
-        case 'Or':
-          if (node.left === undefined) {
-            return node.right;
-          }
+  return fold2(node, (node, visit) => {
+    node = visit(node);
+    switch (node?.name) {
+      case 'And':
+      case 'Or':
+        if (node.left === undefined) {
+          return node.right;
+        }
 
-          if (node.right === undefined) {
-            return node.left;
-          }
+        if (node.right === undefined) {
+          return node.left;
+        }
 
-          return node;
-        case 'Not':
-        case 'Group':
-          if (node.expression === undefined) {
-            return node.expression;
-          }
+        return node;
+      case 'Not':
+      case 'Group':
+        if (node.expression === undefined) {
+          return node.expression;
+        }
 
-          return node;
-        default:
-          return node;
-      }
+        return node;
+      default:
+        return node;
     }
   });
 }
 
 export function removeOffsets(node) {
-  return fold(node, {
-    preVisit(node) {
-      if (node === undefined) {
-        return undefined;
-      }
-
-      if (typeof node !== 'object') {
-        return node;
-      }
-
-      const { start, end, ...rest } = node;
-
-      return new node.constructor(rest);
+  return fold2(node, (node, visit) => {
+    if (node === undefined) {
+      return undefined;
     }
+
+    if (typeof node !== 'object') {
+      return node;
+    }
+
+    const { start, end, ...rest } = node;
+
+    return visit(new node.constructor(rest));
   });
 }
 
 export function fieldGroupsToTermGroups(node) {
-  return fold(node, {
-    postVisit(node) {
-      if (node?.name === 'Term') {
-        const { field, operator, value } = node;
+  return fold2(node, (node, visit) => {
+    if (node?.name === 'Term') {
+      const { field, operator, value } = node;
 
-        switch (field?.name) {
-          case 'And':
-          case 'Or':
-          case 'Not':
-          case 'Group':
-            return fold(field, {
-              postVisit(node) {
-                switch (node?.name) {
-                  case 'Word':
-                  case 'Text':
-                    return new Term({ field: node, operator, value });
-                  default:
-                    return node;
-                }
-              }
-            });
-          default:
-            return node;
-        }
+      switch (field?.name) {
+        case 'And':
+        case 'Or':
+        case 'Not':
+        case 'Group':
+          return fold2(field, (node, visit) => {
+            switch (node?.name) {
+              case 'Word':
+              case 'Text':
+                return new Term({ field: node, operator, value });
+              default:
+                return visit(node);
+            }
+          });
+        default:
+          return visit(node);
       }
-
-      return node;
     }
+
+    return visit(node);
   });
 }
 
 export function valueGroupsToTermGroups(node) {
-  return fold(node, {
-    postVisit(node) {
-      if (node?.name === 'Term') {
-        const { field, operator, value } = node;
+  return fold2(node, (node, visit) => {
+    if (node?.name === 'Term') {
+      const { field, operator, value } = node;
 
-        switch (value?.name) {
-          case 'And':
-          case 'Or':
-          case 'Not':
-          case 'Group':
-            return fold(value, {
-              postVisit(node) {
-                switch (node?.name) {
-                  case 'Word':
-                  case 'Text':
-                    return new Term({ field, operator, value: node });
-                  default:
-                    return node;
-                }
-              }
-            });
-          default:
-            return node;
-        }
+      switch (value?.name) {
+        case 'And':
+        case 'Or':
+        case 'Not':
+        case 'Group':
+          return fold2(value, (node, visit) => {
+            switch (node?.name) {
+              case 'Word':
+              case 'Text':
+                return new Term({ field, operator, value: node });
+              default:
+                return visit(node);
+            }
+          });
+        default:
+          return visit(node);
       }
-
-      return node;
     }
+
+    return visit(node);
   });
 }
 
@@ -209,15 +196,13 @@ export function astFromCst(cst) {
 }
 
 export function queryFromCst(cst) {
-  return fold(cst, {
-    postVisit(node) {
-      switch (node?.name) {
-        case undefined:
-        case 'Word':
-          return node?.raw ?? node?.value ?? '';
-        default:
-          return node.children.join('');
-      }
+  return fold2(cst, (node, visit) => {
+    switch (node?.name) {
+      case undefined:
+      case 'Word':
+        return node?.raw ?? node?.value ?? '';
+      default:
+        return visit(node).children.join('');
     }
   });
 }
